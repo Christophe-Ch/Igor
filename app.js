a506920d719f19b2490ed7931a4ab2e57d2d552b
Christophe Chichmanian <chris.chich@hotmail.fr>
2018-09-28 16:15:49 +0200

    :recycle: Removed commands file

--- app.js
+++ app.js
@@ -78,6 +78,9 @@ client.on("message", async (message) => {
             case 'help':
                 misc.help(message);
                 break;
+
+            case 'or':
+                break;
             
             case 'nextUpdate':
                 bot_management.nextUpdate(message, conf);
